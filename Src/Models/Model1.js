17816a4e46e8ee7ed8f369691027c0c7bb12166d
juanulisesco <alumno26.copati.hugue.ulises@ipm.edu.ar>
2026-03-09 17:48:38 -0300

    Update Model1.js

--- Src/Models/Model1.js
+++ Src/Models/Model1.js
@@ -1,16 +1,32 @@
 
 
-function CrearTarea(id, materia, nota, fecha_creacion, fecha_entrega){
+function CrearTarea(id, materia, nota, fecha_creacion, fecha_entrega, completada){
     return{
         id: id,
         materia: materia,
         nota: nota,
         fecha_creacion: fecha_creacion,
-        fecha_entrega: fecha_entrega
+        fecha_entrega: fecha_entrega,
+        completada : true 
     };
 }
 
 function MostrarTarea(Tarea){
-console.log("Id: " + Tarea.id + ", Materia: " + Tarea.materia + ", Nota: " + Tarea.nota, ", Fecha creacion: " + Tarea.fecha_creacion + ", Fecha entrega: " + Tarea.fecha_entrega);
+    console.log("Id: " + Tarea.id + ", Materia: " +
+        Tarea.materia + ", Nota: " + Tarea.nota, ", Fecha creacion: " +
+        Tarea.fecha_creacion + ", Fecha entrega: " + Tarea.fecha_entrega);
+}
+
+function agregarTarea(lista, tarea) {
+  return [...lista, tarea];
+}
+
+function buscarPorId(lista, id) {
+  return lista.find(tarea => tarea.id === id);
+}
+
+function completarTarea(lista, id) {
+  return lista.map(tarea =>tarea.id === id? { ...tarea, completada: true }: tarea
+  );
 }
 
